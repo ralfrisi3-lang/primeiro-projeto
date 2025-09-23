0ab66c4fa6bc039071a398fc1a6f4f1fca0bbff4
ralfrisi3-lang <ralfrisi3@gmail.com>
2025-09-23 10:46:40 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // 1. Criar variáveis para o nome e a experiência do personagem 
-let nomePersonagem = "Guerreiro"; //alternar entre Mago, Guerreiro, Ladra e Anão para testar
+let nomePersonagem = "Anão"; //alternar entre Mago, Guerreiro, Ladra e Anão para testar
 
-let experienciaPersonagem = 19025; // Pode ser qualquer valor entre 0 e 10000 para testar
+let experienciaPersonagem = 6888; // Pode ser qualquer valor entre 0 e 10000 para testar
 
 // 2. Utilizar uma estrutura de decisão para apresentar mensagens
 if (experienciaPersonagem < 1000) {
-  console.log(`${nomePersonagem}: Ferro`); // Mensagem para < 1000 XP
+  console.log(`${nomePersonagem}: Esta no Nível Ferro`); // Mensagem para < 1000 XP
 } else if (experienciaPersonagem >= 1001 && experienciaPersonagem <= 2000) {
-  console.log(`${nomePersonagem}: Ferro`); // Mensagem para 1001-2000 XP
+  console.log(`${nomePersonagem}: Esta no Nível Ferro`); // Mensagem para 1001-2000 XP
 } else if (experienciaPersonagem >= 2001 && experienciaPersonagem <= 3000) {
-  console.log(`${nomePersonagem}: Bronze`); // Mensagem para 2001-3000 XP
+  console.log(`${nomePersonagem}: Esta no Nível Bronze`); // Mensagem para 2001-3000 XP
 } else if (experienciaPersonagem >= 3001 && experienciaPersonagem <= 4000) {
-  console.log(`${nomePersonagem}: Prata`); // Mensagem para 3001-4000 XP
+  console.log(`${nomePersonagem}: Esta no Nível Prata`); // Mensagem para 3001-4000 XP
 } else if (experienciaPersonagem >= 4001 && experienciaPersonagem <= 5000) {
-  console.log(`${nomePersonagem}: Ouro `); // Mensagem para XP acima de 5000
+  console.log(`${nomePersonagem}: Esta no Nível Ouro `); // Mensagem para XP acima de 5000
 } else if (experienciaPersonagem >= 5001 && experienciaPersonagem <= 6000) {
-  console.log(`${nomePersonagem}: Platina`); // Mensagem para XP acima de 5000
+  console.log(`${nomePersonagem}: Esta no Nível Platina`); // Mensagem para XP acima de 5000
 } else if (experienciaPersonagem >= 6001 && experienciaPersonagem <= 7000) {
-  console.log(`${nomePersonagem}: Ascendente`); //Mensagem para XP acima de 6000 
+  console.log(`${nomePersonagem}: Esta no Nível Ascendente`); //Mensagem para XP acima de 6000 
 } else if (experienciaPersonagem >= 7001 && experienciaPersonagem <= 8000) {
-  console.log(`${nomePersonagem}: Imortal`); //Mensagem para XP acima de 7000  
+  console.log(`${nomePersonagem}: Esta no Nível Imortal`); //Mensagem para XP acima de 7000  
 } else if (experienciaPersonagem >= 8001 && experienciaPersonagem <= 10000) {
-  console.log(`${nomePersonagem}: Radiante das Galaxias`); //Mensagem para XP acima de 8001  
+  console.log(`${nomePersonagem}: Esta no Nível Radiante das Galaxias`); //Mensagem para XP acima de 8001  
 } else if (experienciaPersonagem >= 10001) {
   console.log(`${nomePersonagem}: Atingiu a pontuação maxima!!!`)  
 }
